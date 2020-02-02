6c664d74e47262fa383c5f51cad9ea38c743fe70
joel Alva <panthersjoe@hotmail.com>
2020-02-01 21:45:34 -0500

    form fixes

--- src/components/Footprint/Footprint.js
+++ src/components/Footprint/Footprint.js
@@ -53,55 +53,73 @@ class Footprint extends React.Component {
   render() {
     return (
       <main>
-        <h1>Footprint</h1>
-        <label>
-          name:
-          <input
-            type="text"
-            placeholder="Item Name"
-            value={this.state.newItem}
-            onChange={e => this.updateInput("newItem", e.target.value)}
-          />
-        </label>
-        <label>
-          date
-          <input
-            type="text"
-            placeholder="Date Purchased"
-            value={this.state.date}
-            onChange={e => this.updateInput("date", e.target.value)}
-          />
-        </label>
-        <label>
-          name:
-          <input
-            type="text"
-            placeholder="Purchase Price"
-            value={this.state.purchasePrice}
-            onChange={e => this.updateInput("purchasePrice", e.target.value)}
-          />
-        </label>
-        <label>
-          name:
-          <input
-            type="text"
-            placeholder="Selling Price"
-            value={this.state.sellingPrice}
-            onChange={e => this.updateInput("sellingPrice", e.target.value)}
-          />
-        </label>
-        <button onClick={() => this.addItem()}>add</button>
+        <div className="polar-form">
+          <h1>Footprint</h1>
+          <label>
+            name:
+            <input
+              type="text"
+              placeholder="Item Name"
+              value={this.state.newItem}
+              onChange={e => this.updateInput("newItem", e.target.value)}
+            />
+          </label>
+          <label>
+            Date Purchased
+            <input
+              type="text"
+              placeholder="Date Purchased"
+              value={this.state.date}
+              onChange={e => this.updateInput("date", e.target.value)}
+            />
+          </label>
+          <label>
+            Purchase Price
+            <input
+              type="text"
+              placeholder="Purchase Price"
+              value={this.state.purchasePrice}
+              onChange={e => this.updateInput("purchasePrice", e.target.value)}
+            />
+          </label>
+          <label>
+            Selling Price
+            <input
+              type="text"
+              placeholder="Selling Price"
+              value={this.state.sellingPrice}
+              onChange={e => this.updateInput("sellingPrice", e.target.value)}
+            />
+          </label>
+          <button onClick={() => this.addItem()}>add</button>
+        </div>
         <ul className="list-render">
           {this.state.list.map(item => (
             <li key={item.id}>
-              {item.newItem}
-
-              {item.date}
-
-              {item.purchasePrice}
-
-              {item.sellingPrice}
-              <button onClick={() => this.deleteItem(item.id)}>X</button>
+              <div className="master-items">
+                <div className="list-items">
+                  <p>Product Name</p>
+                  {item.newItem}
+                </div>
+                <div className="list-items">
+                  <p>Date Purchased</p>
+                  {item.date}
+                </div>
+                <div className="list-items">
+                  <p>Purchase Price</p>
+                  {item.purchasePrice}
+                </div>
+                <div className="list-items">
+                  <p>Selling Price</p>
+                  {item.sellingPrice}
+                </div>
+                <button
+                  className="delete-button"
+                  onClick={() => this.deleteItem(item.id)}
+                >
+                  X
+                </button>
+              </div>
             </li>
           ))}
         </ul>
