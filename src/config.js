19bbe2228c937eaa4c2bcc4263bd1c5440821bfc
joel Alva <panthersjoe@hotmail.com>
2020-03-22 16:37:27 -0400

    changed heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://gentle-lake-71753.herokuapp.com`,
+  API_ENDPOINT: `https://blooming-mountain-59788.herokuapp.com`,
   // API_ENDPOINT: "http://localhost:8000/api",
   TOKEN_KEY: "polar-client-auth-token"
 };
