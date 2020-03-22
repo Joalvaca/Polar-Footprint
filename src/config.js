b88b0e661a5463058dc19e4eb9d878ff7a2e744c
joel Alva <panthersjoe@hotmail.com>
2020-03-22 18:08:41 -0400

    added api to endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://agile-depths-11880.herokuapp.com`,
+  API_ENDPOINT: `https://agile-depths-11880.herokuapp.com/api`,
   // API_ENDPOINT: "http://localhost:8000/api",
   TOKEN_KEY: "polar-client-auth-token"
 };
