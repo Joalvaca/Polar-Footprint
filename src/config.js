78413c9700128f2c4b59e5143732283dc9b627ce
joel Alva <panthersjoe@hotmail.com>
2020-03-22 03:12:08 -0400

    fixed endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: `https://git.heroku.com/gentle-lake-71753.git`,
+  API_ENDPOINT: `https://gentle-lake-71753.herokuapp.com/`,
   // API_ENDPOINT: "http://localhost:8000/api",
   TOKEN_KEY: "polar-client-auth-token"
 };
